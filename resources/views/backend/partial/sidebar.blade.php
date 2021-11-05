4ac2c3c9480bdf681abc9887ece208a618774213
judemuego <47876710+judemuego@users.noreply.github.com>
2021-11-05 13:26:39 +0800

    push

--- resources/views/backend/partial/sidebar.blade.php
+++ resources/views/backend/partial/sidebar.blade.php
@@ -21,15 +21,88 @@
                     <i class="align-middle mr-2 fas fa-fw fa-home"></i> <span class="align-middle">SMP</span>
                 </a>
                 <ul id="dashboards" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
-                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-default.html">About Company</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-analytics.html">Contact Information</a></li>
-                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Core Values</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-default.html">Administrator Page</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-analytics.html">Editing Page</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Analytics/Dashboards</a></li>
 
                 </ul>
             </li>
             <li class="sidebar-item">
+                <a href="#purchasing" data-toggle="collapse" class="sidebar-link collapsed">
+                    <i class="align-middle mr-2 fas fa-fw fa-home"></i> <span class="align-middle">Procurement</span>
+                </a>
+                <ul id="purchasing" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-default.html">Purchase Requisition</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-analytics.html">Approval</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Purchase Order</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Goods/Service Receipt</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Invoice Match/Payment</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Identify Need</a></li>
+                </ul>
+            </li>
+            <li class="sidebar-item">
+                <a href="#inventory" data-toggle="collapse" class="sidebar-link collapsed">
+                    <i class="align-middle mr-2 fas fa-fw fa-home"></i> <span class="align-middle">Inventory System</span>
+                </a>
+                <ul id="inventory" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-default.html">Product Inventory</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-analytics.html">Product Purchasing Tracking</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Product Sales</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">RMA and RTV</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Product Inventory ATP and CTP</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Product Expiration</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Inventory Sales Report</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Reports</a></li>
+
+                </ul>
+            </li>
+            <li class="sidebar-item">
+                <a href="#payroll" data-toggle="collapse" class="sidebar-link collapsed">
+                    <i class="align-middle mr-2 fas fa-fw fa-home"></i> <span class="align-middle">Payroll System</span>
+                </a>
+                <ul id="payroll" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-default.html">Employee Profile</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-analytics.html">Attendance</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Payment</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Advance Payment</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Deduction</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Salary Management</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Overtime</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Salary Slips</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Biometrics</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Reports</a></li>
+                </ul>
+            </li>
+            <li class="sidebar-item">
+                <a href="#accounting" data-toggle="collapse" class="sidebar-link collapsed">
+                    <i class="align-middle mr-2 fas fa-fw fa-home"></i> <span class="align-middle">Accounting System</span>
+                </a>
+                <ul id="accounting" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-default.html">General Accounts</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-analytics.html">Sales</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Purchasing</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Inventory</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Customer</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Suppliers</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Fixed Assets</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Reports</a></li>
+                </ul>
+            </li>
+            <li class="sidebar-item">
+                <a href="#sysad" data-toggle="collapse" class="sidebar-link collapsed">
+                    <i class="align-middle mr-2 fas fa-fw fa-home"></i> <span class="align-middle">System Administration</span>
+                </a>
+                <ul id="sysad" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-default.html">User Management</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-analytics.html">User Roles and Permissions</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Viewing and Updating of Employee Info</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">Viewing of Dashbaards</a></li>
+                    <li class="sidebar-item"><a class="sidebar-link" href="dashboard-e-commerce.html">User Maintenance</a></li>
+                </ul>
+            </li>
+            <li class="sidebar-item">
                 <a href="#pages" data-toggle="collapse" class="sidebar-link collapsed">
-        <i class="align-middle mr-2 fas fa-fw fa-file"></i> <span class="align-middle">Maintenance</span>
+        <i class="align-middle mr-2 fas fa-fw fa-file"></i> <span class="align-middle">Website Maintenance</span>
         </a>
                 <ul id="pages" class="sidebar-dropdown list-unstyled collapse " data-parent="#sidebar">
                     <li class="sidebar-item"><a class="sidebar-link" href="pages-settings.html">Projects</a></li>
